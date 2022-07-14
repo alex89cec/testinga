5825c782d0d87b883d4825ad913c1c6ea5c5bd08
El Deivi <eldeivi@me>
2022-07-14 14:38:00 -0300

    [main] bla

--- dashboardList .py
+++ dashboardList .py
@@ -1,48 +1,52 @@
-from getpass import getuser
-from tkinter import Y
-from typing import List
+import os
 import requests
 import json
-import csv
 
-tokenSfx = "AAAAAAAAAAAAAAAA"
+tokenSfx = os.getenv("SFX_AUTH_TOKEN")
 headerSfx = "X-SF-TOKEN"
-baseUrl= "https://api.us1.signalfx.com/v2"
+baseUrl = "https://api.us1.signalfx.com/v2"
 header = ['name', 'DashboardGroup', 'creator']
 data = []
 
 
-def getUserList ():
-    response = requests.get(baseUrl+"/organization/member",headers = {headerSfx: tokenSfx})
-    list = json.loads(response.text)["results"]
-    
-    return list
-
-userList = getUserList()
-
-def getName (id):
-
-    for user in userList:
-        if user["userId"] == id:
-            return user["fullName"]
-        elif "AAAAAAAAAAA" == id:
-            return "SignalFx Created"
-    
-    return "Not Find"
+def get_users():
+    response = requests.get(baseUrl + "/organization/member", headers={headerSfx: tokenSfx})
+    return json.loads(response.text)["results"]
 
 
+def get_dashboards():
+    offset = 0
+    limit = 100
+    results = []
+    while True:
+        response = requests.get(f"{baseUrl}/dashboard?limit={limit}&offset={offset}", headers={headerSfx: tokenSfx})
+        response.raise_for_status()
+        answer = json.loads(response.text)
+        results += answer["results"]
+        offset += limit
+        if offset >= answer["count"]:
+            break
 
-def getList():
-    response = requests.get(baseUrl+"/dashboard",headers = {headerSfx: tokenSfx})
-    answer = json.loads(response.text)
-    return answer["results"]
+    # Remove charts that does not have charts?
+    return list(filter(lambda x: len(x["charts"]) > 0, results))
 
 
+def get_name(users, id):
+    if "AAAAAAAAAAA" == id:
+        return "SignalFx Created"
+
+    found_user = next((u["fullName"] for u in users if u["userId"] == id), None)
+    return found_user if found_user else f"User not found {id}"
 
 
-
-list = getList()
-print(list)
-for dashboard in list:
-    #print( dashboard["name"]+","+dashboard["groupName"]+","+getName(dashboard["creator"])+","+dashboard["creator"])
-    print( dashboard["name"]+","+dashboard["groupName"]+","+getName(dashboard["creator"]))
+users = get_users()
+dashboards = get_dashboards()
+print(f"We have {len(dashboards)} dashboards to migrate")
+for dashboard in dashboards:
+    creator_name = get_name(users, dashboard['creator'])
+    directory = f"./dashboards/{creator_name}"
+    print(f"{dashboard['name']},{dashboard['groupName']},{creator_name}")
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    with open(f"{directory}/{dashboard['name']}.json", 'w') as outfile:
+        outfile.write(json.dumps(dashboard))
